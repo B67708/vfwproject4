abc5a62de95790218e6ea48be5150c7adca84412
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-03-25 17:17:14 -0400

    Created json file and setup variable.

---
+++ JS/json.js
@@ -0,0 +1,36 @@
+var json = {
+	"Vehicle1": {
+		"newUsed" : ["New or Used:" , "New"],
+		"make" : ["Make:" , "Chevrolet"],
+		"model" : ["Model:" , "Cobalt"],
+		"year" : ["Year:" , "2013"],
+		"mileage" : ["Mileage:" , "3"],
+		"condition" : ["Condition:" , "5"],
+		"date" : ["Date:" , "3/25/2013"],
+		"notes" : ["Notes:" , "Brand new. Loaded SS model."],
+	}
+};
+
+	"Vehicle2": {
+		"newUsed" : ["New or Used:" , "New"],
+		"make" : ["Make:" , "Ford"],
+		"model" : ["Model:" , "Focus"],
+		"year" : ["Year:" , "2013"],
+		"mileage" : ["Mileage:" , "5"],
+		"condition" : ["Condition:" , "5"],
+		"date" : ["Date:" , "3/25/2013"],
+		"notes" : ["Notes:" , "Brand new. Great gas mileage."],
+	}
+};
+
+	"Vehicle3": {
+		"newUsed" : ["New or Used:" , "New"],
+		"make" : ["Make:" , "Dodge"],
+		"model" : ["Model:" , "Viper"],
+		"year" : ["Year:" , "2013"],
+		"mileage" : ["Mileage:" , "1"],
+		"condition" : ["Condition:" , "5"],
+		"date" : ["Date:" , "3/25/2013"],
+		"notes" : ["Notes:" , "Brand new. FAST!"],
+	}
+};
